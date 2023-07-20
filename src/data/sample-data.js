e0304dbd79c2076ba63848511d16e2514cb94ff1
Nikhil-13 <nnik270@gmail.com>
2023-07-20 19:07:09 +0530

    changes

---
+++ src/data/sample-data.js
@@ -0,0 +1,39 @@
+export const cityList = {
+	cities: [
+		{
+			name: 'Mumbai',
+			temperature: 30,
+			humidity: 75,
+			wind_speed: 10,
+			aqi: 220,
+		},
+		{
+			name: 'Delhi',
+			temperature: 35,
+			humidity: 65,
+			wind_speed: 15,
+			aqi: 450,
+		},
+		{
+			name: 'Bangalore',
+			temperature: 28,
+			humidity: 80,
+			wind_speed: 8,
+			aqi: 320,
+		},
+		{
+			name: 'Chennai',
+			temperature: 32,
+			humidity: 70,
+			wind_speed: 12,
+			aqi: 180,
+		},
+		{
+			name: 'Kolkata',
+			temperature: 33,
+			humidity: 70,
+			wind_speed: 10,
+			aqi: 200,
+		},
+	],
+}
